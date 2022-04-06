4a1c3a92b9bcc4921508f7f521646b5b328ae1fa
abdulnoor <abdulnoormostafa@gmail.com>
2022-04-06 21:12:49 +0300

    complete

--- project.js
+++ project.js
@@ -1,14 +1,14 @@
-let twoSum = function(nums, target) 
+let twoSum = function ( nums, target ) 
 {
-    for(let i = 0; i <= nums.length; i++)
+    for( let i = 0; i <= nums.length; i++ )
     {
-        for(let j = 0; j <= nums.length; j++)
+        for( let j = 0; j <= nums.length; j++ )
         {
-            if(nums[i] + nums[j] == target)
+            if( nums[i] + nums[j] == target )
             {
                 return [i, j]
             }
         }
     }
 };
-twoSum(1,3);
+console.log( twoSum( [15, 7, 11, 2] , 18 ) );
